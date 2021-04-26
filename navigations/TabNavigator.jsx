9c7d1c19ac77f6c24e6d8d4b6e7af053f065d123
dhmgmhw <dhmgmhw@gmail.com>
2021-04-26 17:39:37 +0900

    rename : rename

--- navigations/TabNavigator.jsx
+++ navigations/TabNavigator.jsx
@@ -4,9 +4,8 @@ import { Platform } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import HomeMain from '../pages/home/HomeMain';
-import ServiceMain from '../pages/ourservice/ServiceMain';
+import ChatMain from '../pages/chat/ChatMain';
 import SearchMain from '../pages/search/SearchMain';
-import History from '../pages/history/HistoryMain';
 import MyPageMain from '../pages/mypage/MyPageMain';
 
 import { Ionicons } from '@expo/vector-icons';
@@ -21,14 +20,12 @@ const TabNavigator = () => {
           let iconName = Platform.OS === 'ios' ? 'ios-' : 'md-';
           if (route.name === 'HomeMain') {
             iconName += 'home-outline';
-          } else if (route.name === 'ServiceMain') {
-            iconName += 'library-outline';
           } else if (route.name === 'SearchMain') {
             iconName += 'search-outline';
-          } else if (route.name === 'History') {
-            iconName += 'reader-outline';
+          } else if (route.name === 'ChatMain') {
+            iconName += 'chatbox-outline';
           } else if (route.name === 'MyPageMain') {
-            iconName += 'person';
+            iconName += 'library-outline';
           }
           return (
             <Ionicons
@@ -49,9 +46,8 @@ const TabNavigator = () => {
         },
       }}>
       <Tabs.Screen name='HomeMain' component={HomeMain} />
-      <Tabs.Screen name='ServiceMain' component={ServiceMain} />
       <Tabs.Screen name='SearchMain' component={SearchMain} />
-      <Tabs.Screen name='History' component={History} />
+      <Tabs.Screen name='ChatMain' component={ChatMain} />
       <Tabs.Screen name='MyPageMain' component={MyPageMain} />
     </Tabs.Navigator>
   );
--- pages/chat/ChatMain.jsx
+++ pages/chat/ChatMain.jsx
@@ -2,10 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function ServiceMain() {
+export default function ChatMain() {
   return (
     <View style={styles.container}>
-      <Text>Open up ServiceMain.jsx to start working on your app!</Text>
+      <Text>Open up ChatMain.jsx to start working on your app!</Text>
       <StatusBar style='auto' />
     </View>
   );
