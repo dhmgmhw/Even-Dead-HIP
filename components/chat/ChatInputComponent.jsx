01aa3a0c4689a0cf8462d465505e3c0ad2a423a5
dhmgmhw <dhmgmhw@gmail.com>
2021-04-28 16:18:08 +0900

    add: chat Page

---
+++ components/chat/ChatInputComponent.jsx
@@ -0,0 +1,102 @@
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Dimensions,
+  TextInput,
+  KeyboardAvoidingView,
+} from 'react-native';
+import { Item } from 'native-base';
+
+import { Ionicons } from '@expo/vector-icons';
+
+const diviceWidth = Dimensions.get('window').width;
+const diviceHeight = Dimensions.get('window').height;
+
+export default function ChatInputComponent() {
+  const [text, setText] = useState('메시지를 입력하세요');
+
+  const leaveMessage = async () => {
+    await console.log('Hey');
+  };
+
+  return (
+    <KeyboardAvoidingView style={styles.messageBar} behavior='position'>
+      <Item style={styles.messageBox}>
+        <TextInput
+          style={{
+            paddingLeft: 20,
+            backgroundColor: '#eeeeee',
+            margin: 7,
+            height: 40,
+            borderWidth: 1,
+            borderColor: 'lightgrey',
+            borderRadius: 100,
+            width: diviceWidth * 0.8,
+          }}
+          onChangeText={setText}
+          value={text}
+          placeholder='메시지를 입력하세요'
+        />
+        <Ionicons
+          active
+          onPress={leaveMessage}
+          name='chatbox'
+          size={27}
+          style={{ marginHorizontal: 10, color: '#202540' }}
+        />
+      </Item>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { backgroundColor: 'white' },
+  chatInfoBox: {
+    width: diviceWidth,
+    height: 80,
+    borderWidth: 2,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  chatBox: {
+    width: diviceWidth,
+    height: 80,
+    borderWidth: 2,
+    marginTop: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  userBox: {
+    width: 60,
+    height: 60,
+    borderRadius: 100,
+    backgroundColor: 'grey',
+  },
+  descBox: {
+    width: '60%',
+    height: 70,
+    backgroundColor: 'lightgrey',
+  },
+  bookBox: {
+    width: 60,
+    height: 70,
+    backgroundColor: 'grey',
+  },
+  messageBar: {
+    width: diviceWidth,
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+  messageBox: {
+    width: diviceWidth,
+    height: 45,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    backgroundColor: '#E4E4E4',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+});
--- pages/chat/ChatPage.jsx
+++ pages/chat/ChatPage.jsx
@@ -6,56 +6,69 @@ import {
   Dimensions,
   ScrollView,
   Pressable,
+  TextInput,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from 'react-native';
 import { Header } from 'react-native-elements';
 
 import { Ionicons } from '@expo/vector-icons';
+import ChatInputComponent from '../../components/chat/ChatInputComponent';
 
 const diviceWidth = Dimensions.get('window').width;
 const diviceHeight = Dimensions.get('window').height;
 
 export default function ChatPage({ navigation }) {
   return (
-    <>
-      <Header
-        placement='left'
-        containerStyle={{
-          backgroundColor: 'white',
-          alignSelf: 'center',
-          borderBottomWidth: 1,
-        }}
-        leftComponent={
-          <Ionicons
-            onPress={() => {
-              navigation.goBack();
-            }}
-            name={'chevron-back'}
-            size={27}
-            color={'black'}
-          />
-        }
-        centerComponent={''}
-        rightComponent={
-          <Ionicons
-            name={'search'}
-            size={27}
-            style={{ marginHorizontal: 10 }}
-          />
-        }
-      />
-      <View style={styles.chatInfoBox}>
-        <View style={styles.bookBox}></View>
-        <View style={styles.userBox}></View>
-        <View style={styles.descBox}></View>
-      </View>
-      <ScrollView style={styles.container}>
-        <View style={styles.chatBox}>
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}>
+      <>
+        <Header
+          placement='left'
+          containerStyle={{
+            backgroundColor: 'white',
+            alignSelf: 'center',
+            borderBottomWidth: 1,
+          }}
+          leftComponent={
+            <Ionicons
+              onPress={() => {
+                navigation.goBack();
+              }}
+              name={'chevron-back'}
+              size={27}
+              color={'black'}
+            />
+          }
+          centerComponent={''}
+          rightComponent={
+            <Ionicons
+              name={'search'}
+              size={27}
+              style={{ marginHorizontal: 10 }}
+            />
+          }
+        />
+        <View style={styles.chatInfoBox}>
+          <View style={styles.bookBox}></View>
           <View style={styles.userBox}></View>
           <View style={styles.descBox}></View>
-          <View style={styles.bookBox}></View>
         </View>
-      </ScrollView>
-    </>
+        <ScrollView style={styles.container}>
+          <View style={styles.oppositeChatBox}>
+            <View style={styles.oppositeUserBox}></View>
+            <View style={styles.oppositeTextBox}></View>
+          </View>
+          <View style={styles.myChatBox}>
+            <View style={styles.myTextBox}></View>
+          </View>
+        </ScrollView>
+        <ChatInputComponent />
+      </>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -65,14 +78,6 @@ const styles = StyleSheet.create({
     width: diviceWidth,
     height: 80,
     borderWidth: 2,
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  chatBox: {
-    width: diviceWidth,
-    height: 80,
-    borderWidth: 2,
-    marginTop: 10,
     flexDirection: 'row',
     alignItems: 'center',
   },
@@ -92,4 +97,33 @@ const styles = StyleSheet.create({
     height: 70,
     backgroundColor: 'grey',
   },
+  oppositeChatBox: {
+    width: diviceWidth,
+    height: 80,
+    borderWidth: 2,
+    marginTop: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  myChatBox: {
+    width: diviceWidth,
+    height: 80,
+    borderWidth: 2,
+    marginTop: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  oppositeUserBox: {
+    width: 60,
+    height: 60,
+    borderRadius: 100,
+    backgroundColor: 'grey',
+  },
+  oppositeTextBox: {
+    width: '60%',
+    height: 70,
+    backgroundColor: 'lightgrey',
+  },
+  myTextBox: { width: '60%', height: 70, backgroundColor: 'lightgrey' },
 });
