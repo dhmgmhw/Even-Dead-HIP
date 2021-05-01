126fc3b2e128582a2d8e451949ea6168640a9f53
dhmgmhw <dhmgmhw@gmail.com>
2021-05-01 15:54:44 +0900

    add: kakao result component

---
+++ components/home/KakaoResultCardComponent.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function KakaoResultCardComponent({ book }) {
+  const bookCover =
+    book.thumbnail == ''
+      ? require('../../assets/basicbookcover.png')
+      : { uri: book.thumbnail };
+  return (
+    <View style={styles.container}>
+      <Image
+        style={{ width: 90, height: 130 }}
+        resizeMode='contain'
+        source={bookCover}
+      />
+      <View style={{ padding: 15 }}>
+        <Text numberOfLines={2} ellipsizeMode={'tail'} style={styles.title}>
+          {book.title}
+        </Text>
+        <Text numberOfLines={1} ellipsizeMode={'tail'} style={styles.publisher}>
+          {book.publisher}
+        </Text>
+        <Text numberOfLines={1} ellipsizeMode={'tail'} style={styles.authors}>
+          {book.authors}
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    marginBottom: 15,
+  },
+  title: { fontSize: 15, width: 200, fontFamily: 'SCDream6', marginBottom: 15 },
+  publisher: { fontFamily: 'SCDream3', marginBottom: 15 },
+  authors: { fontFamily: 'SCDream5' },
+});
