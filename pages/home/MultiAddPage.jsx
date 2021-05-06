97948c4461cf7a8fff9c9ddba5418edc70395ac6
dhmgmhw <dhmgmhw@gmail.com>
2021-05-06 21:36:11 +0900

    feat: multi picker

--- pages/home/MultiAddPage.jsx
+++ pages/home/MultiAddPage.jsx
@@ -1,32 +1,31 @@
-// import * as React from "react"
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from 'react';
 
-import { Text, View, StyleSheet, SafeAreaView, Alert } from "react-native"
-import { AssetsSelector } from "expo-images-picker"
-import { Ionicons } from "@expo/vector-icons"
-import StatusBarPlaceHolder from "../../components/AddPage/StatusBarPlaceHolder"
-import { SafeAreaProvider } from "react-native-safe-area-context"
-import CustomNavigator from "../../components/AddPage/CustomTopNavigator"
+import { Text, View, StyleSheet, SafeAreaView, Alert } from 'react-native';
+import { AssetsSelector } from 'expo-images-picker';
+import { Ionicons } from '@expo/vector-icons';
+import StatusBarPlaceHolder from '../../components/AddPage/StatusBarPlaceHolder';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import CustomNavigator from '../../components/AddPage/CustomTopNavigator';
 
 const ForceInset = {
-  top: "never",
-  bottom: "never",
-}
+  top: 'never',
+  bottom: 'never',
+};
 
-export default function MultiAddPage(navigation) {
-  const onDone = data => {
-    navigation.navigate("AddPage", setimage)
-    Alert.alert("Selected items are", JSON.stringify(data))
-    console.log(JSON.stringify(data))
-    console.log(data)
-    console.log(doneFunction)
-    console.log(onDone)
-  }
+export default function MultiAddPage({ navigation, route }) {
+  const func = route.params;
+
+  useEffect(() => {}, []);
+
+  const onDone = (data) => {
+    func(data);
+    // console.log(data);
+  };
 
   const goBack = () => {
-    console.log("Going back use Navigator goBack() hook")
-  }
-  const [image, setimage] = useState(JSON.stringify(data))
+    navigation.pop();
+  };
+  const [image, setimage] = useState();
 
   return (
     <SafeAreaProvider>
@@ -35,39 +34,45 @@ export default function MultiAddPage(navigation) {
         <View style={styles.container}>
           <AssetsSelector
             options={{
-              assetsType: ["photo", "video"],
-              maxSelections: 10,
+              manipulate: {
+                // width: 200,
+                // height: 200,
+                compress: 0.5,
+              },
+              assetsType: ['photo'],
+              maxSelections: 5,
               margin: 2,
               portraitCols: 4,
               landscapeCols: 5,
               widgetWidth: 100,
-              widgetBgColor: "white",
+              widgetBgColor: 'white',
               videoIcon: {
                 Component: Ionicons,
-                iconName: "ios-videocam",
-                color: "tomato",
+                iconName: 'videocam',
+                color: 'tomato',
                 size: 20,
               },
               selectedIcon: {
                 Component: Ionicons,
-                iconName: "ios-checkmark-circle-outline",
-                color: "white",
-                bg: "#0eb14970",
+                iconName: 'checkmark-circle-outline',
+                color: 'white',
+                bg: '#0eb14970',
                 size: 26,
               },
-              spinnerColor: "black",
+              spinnerColor: 'grey',
               onError: () => {},
               noAssets: () => <View></View>,
               defaultTopNavigator: {
-                continueText: "Finish",
-                goBackText: "Back",
-                selectedText: "Selected",
-                midTextColor: "tomato",
-                buttonStyle: _buttonStyle,
+                continueText: '선택완료',
+                goBackText: '뒤로',
+                selectedText: 'Selected',
+                midTextColor: 'blue',
                 buttonTextStyle: _textStyle,
                 backFunction: goBack,
-
-                doneFunction: data => onDone(data),
+                doneFunction: (data) => {
+                  onDone(data);
+                  navigation.pop();
+                },
               },
             }}
           />
@@ -75,20 +80,21 @@ export default function MultiAddPage(navigation) {
         <Text>{image}</Text>
       </SafeAreaView>
     </SafeAreaProvider>
-  )
+  );
 }
 
 const _textStyle = {
-  color: "white",
-}
+  color: 'black',
+  fontFamily: 'SCDream5',
+};
 const _buttonStyle = {
-  backgroundColor: "black",
+  backgroundColor: 'black',
   borderRadius: 18,
-}
+};
 
 // if you want to use defaultTopNavigator you must send in basic style
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-})
+});
