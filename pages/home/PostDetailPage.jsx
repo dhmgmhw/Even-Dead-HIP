63ce07a9d80e2c8420592ce3c75e22b81ee97b2c
dhmgmhw <dhmgmhw@gmail.com>
2021-05-08 23:14:55 +0900

    style: detailpage design

--- pages/home/PostDetailPage.jsx
+++ pages/home/PostDetailPage.jsx
@@ -7,10 +7,17 @@ import {
   ImageBackground,
   Dimensions,
   Pressable,
+  TextInput,
+  TouchableWithoutFeedback,
+  KeyboardAvoidingView,
 } from 'react-native';
-import { Header, Image } from 'react-native-elements';
+import { Header, Image, Tooltip } from 'react-native-elements';
 
 import { Ionicons } from '@expo/vector-icons';
+
+import Swiper from 'react-native-swiper-hooks';
+import CommentBubbleComponent from '../../components/home/CommentBubbleComponent';
+import { deletePost, postComment } from '../../config/PostingApi';
 
 const diviceWidth = Dimensions.get('window').width;
 const diviceHeight = Dimensions.get('window').height;
@@ -18,89 +25,232 @@ const diviceHeight = Dimensions.get('window').height;
 export default function PostDetailPage({ navigation, route }) {
   const detailData = route.params;
 
-  const [heart, setHeart] = useState(false);
-  const [scrap, setScrap] = useState(false);
-
-  const heartToggle = () => {
-    heart ? setHeart(false) : setHeart(true);
-  };
-  const scrapToggle = () => {
-    scrap ? setScrap(false) : setScrap(true);
-  };
-
   const image = { uri: detailData.image };
+  const [comment, setComment] = useState('');
 
-  // useLayoutEffect(() => {
-  //   console.log(getStatusBarHeight());
-  // }, []);
+  useLayoutEffect(() => {
+    console.log(detailData);
+  }, []);
 
   return (
-    <ImageBackground
-      source={image}
-      blurRadius={3}
-      style={styles.imageBackground}
-      imageStyle={{ opacity: 0.3 }}>
-      <ScrollView bounces={false}>
-        <Header
-          barStyle='light-content'
-          containerStyle={{
-            backgroundColor: 'transparent',
-            borderBottomWidth: 0,
-            alignSelf: 'center',
-          }}
-          leftComponent={
-            <Ionicons
-              onPress={() => {
-                navigation.goBack();
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}>
+      <ImageBackground
+        source={image}
+        blurRadius={3}
+        style={styles.imageBackground}
+        imageStyle={{ opacity: 0.3 }}>
+        <KeyboardAvoidingView behavior='position'>
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <Header
+              barStyle='light-content'
+              containerStyle={{
+                backgroundColor: 'transparent',
+                borderBottomWidth: 0,
+                alignSelf: 'center',
               }}
-              name={'chevron-back'}
-              size={27}
-              color={'white'}
+              leftComponent={
+                <Ionicons
+                  onPress={() => {
+                    navigation.goBack();
+                  }}
+                  name={'chevron-back'}
+                  size={27}
+                  color={'white'}
+                />
+              }
+              centerComponent={''}
+              rightComponent={
+                <Ionicons
+                  name={'open-outline'}
+                  size={27}
+                  color={'white'}
+                  style={{ marginHorizontal: 10 }}
+                />
+              }
             />
-          }
-          centerComponent={''}
-          rightComponent={
-            <View style={{ flexDirection: 'row' }}>
-              <Ionicons
-                onPress={heartToggle}
-                name={heart ? 'heart' : 'heart-outline'}
-                size={27}
-                color={heart ? 'red' : 'white'}
-                style={{ marginHorizontal: 10 }}
-              />
-              <Ionicons
-                onPress={scrapToggle}
-                name={scrap ? 'bookmark' : 'bookmark-outline'}
-                color={scrap ? '#4C65FF' : 'white'}
-                size={27}
-              />
+            <Swiper
+              height={340}
+              width={210}
+              showPagination={true}
+              paginationSelectedColor={'#438732'}
+              paginationPosition={'bottom'}
+              autoplay={false}
+              loop={false}
+              paginationSelectedSize={8}
+              outerContainerStyle={{
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginBottom: 40,
+                marginTop: 20,
+              }}>
+              <View style={styles.bookImageBox}>
+                <Image
+                  style={styles.bookImage}
+                  resizeMode='cover'
+                  source={{ uri: detailData.image }}
+                />
+              </View>
+              <View style={styles.bookImageBox}>
+                <Image
+                  style={styles.bookImage}
+                  resizeMode='contain'
+                  source={{ uri: detailData.captureImages[0] }}
+                />
+              </View>
+              {detailData.captureImages[1] ? (
+                <View style={styles.bookImageBox}>
+                  <Image
+                    style={styles.bookImage}
+                    resizeMode='contain'
+                    source={{ uri: detailData.captureImages[1] }}
+                  />
+                </View>
+              ) : null}
+              {detailData.captureImages[2] ? (
+                <View style={styles.bookImageBox}>
+                  <Image
+                    style={styles.bookImage}
+                    resizeMode='contain'
+                    source={{ uri: detailData.captureImages[2] }}
+                  />
+                </View>
+              ) : null}
+            </Swiper>
+            <View style={styles.container}>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                }}>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                  <Image
+                    style={{
+                      height: 55,
+                      width: 55,
+                      borderRadius: 100,
+                      marginRight: 15,
+                    }}
+                    resizeMode='cover'
+                    source={{ uri: detailData.image }}
+                  />
+                  <View>
+                    <Text
+                      style={{
+                        fontSize: 17,
+                        fontFamily: 'SCDream7',
+                        marginBottom: 5,
+                      }}>
+                      나는 곰돌이푸
+                    </Text>
+                    <Text style={{ fontSize: 13, fontFamily: 'SCDream5' }}>
+                      송파구
+                    </Text>
+                  </View>
+                </View>
+                <Tooltip
+                  withOverlay={false}
+                  containerStyle={{
+                    height: 60,
+                    backgroundColor: '#438732',
+                  }}
+                  pointerColor={'#438732'}
+                  popover={
+                    <>
+                      <Pressable
+                        style={styles.tooltipBtn}
+                        onPress={() => {
+                          navigation.pop();
+                          navigation.navigate('PostFixPage', detailData);
+                        }}>
+                        <Text style={styles.tooltipText}>수정</Text>
+                      </Pressable>
+                      <View
+                        style={{
+                          height: 1,
+                          backgroundColor: 'white',
+                          width: 100,
+                        }}></View>
+                      <Pressable
+                        style={styles.tooltipBtn}
+                        onPress={() => {
+                          deletePost(detailData.id);
+                          alert('게시글을 삭제했습니다!');
+                        }}>
+                        <Text style={styles.tooltipText}>삭제</Text>
+                      </Pressable>
+                    </>
+                  }>
+                  <Ionicons
+                    name={'ellipsis-vertical'}
+                    color={'black'}
+                    size={27}
+                  />
+                </Tooltip>
+              </View>
+              <View style={styles.descMiddleBorder}></View>
+              <Text style={styles.bookCate}>#{detailData.category}</Text>
+              <Text style={styles.bookTitle} numberOfLines={2}>
+                {detailData.title}
+              </Text>
+              <Text style={styles.bookAuthor}>{detailData.author}</Text>
+              <View style={styles.descMiddleBorder}></View>
+              <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <Text style={styles.subTitle}>도서상태</Text>
+                <View style={styles.bookStatusBadge}>
+                  <Text
+                    style={{
+                      fontFamily: 'SCDream5',
+                      fontSize: 18,
+                      color: '#54b65e',
+                      top: 1,
+                    }}>
+                    {detailData.status}
+                  </Text>
+                </View>
+              </View>
+              <Text style={styles.bookDesc}>{detailData.contentInfo}</Text>
+              <View style={styles.descMiddleBorder}></View>
+              <Text style={styles.subTitle}>작품소개</Text>
+              <Text style={styles.bookDesc}>{detailData.description}</Text>
+              {/* <Text style={{ textAlign: 'right' }}>하이퍼링크</Text> */}
+              <View style={styles.descMiddleBorder}></View>
+              <Text style={styles.subTitle}>댓글</Text>
+
+              <View style={styles.commentBox}>
+                <CommentBubbleComponent />
+                <View style={styles.commentInputBox}>
+                  <TextInput
+                    style={styles.commentInput}
+                    onChangeText={setComment}
+                    value={comment}
+                    placeholder='| 이 책에 대한 가치를 같이 나눠봐요.'
+                    placeholderTextColor={'white'}
+                  />
+                  <Pressable
+                    onPress={async () => {
+                      await postComment(comment, detailData.id);
+                    }}
+                    style={styles.commentBtn}>
+                    <Text style={styles.commentBtnText}>입력</Text>
+                  </Pressable>
+                </View>
+              </View>
             </View>
-          }
-        />
-        <View style={styles.bookImageBox}>
-          <Image style={styles.bookImage} resizeMode='cover' source={image} />
-        </View>
-        <View style={styles.container}>
-          <Text style={styles.bookCate}>카테고리 {'>'} 인문</Text>
-          <Text style={styles.bookTitle}>{detailData.title}</Text>
-          <Text style={styles.bookAuthor}>{detailData.author}</Text>
-          <View style={styles.descMiddleBorder}></View>
-          <Text style={styles.subTitle}>작품소개</Text>
-          <Text style={styles.bookDesc}>{detailData.description}</Text>
-          <Text style={styles.subTitle}>도서추천</Text>
-          <Text style={styles.bookDesc}>
-            여러분 꼭 한번 시간내서 보십쇼 정말 재밌습니다...
-          </Text>
-        </View>
-      </ScrollView>
-      <Pressable
-        style={styles.chatBox}
-        onPress={() => {
-          navigation.navigate('ChatPage');
-        }}>
-        <Text style={styles.chatBtnText}>채팅연결하기</Text>
-      </Pressable>
-    </ImageBackground>
+          </ScrollView>
+          {/* <Pressable
+            style={styles.chatBox}
+            onPress={() => {
+              navigation.navigate('ChatPage');
+            }}>
+            <Text style={styles.chatBtnText}>가치 교환하기</Text>
+          </Pressable> */}
+        </KeyboardAvoidingView>
+      </ImageBackground>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -112,58 +262,111 @@ const styles = StyleSheet.create({
     backgroundColor: 'black',
   },
   bookImageBox: {
-    height: 250,
-    width: diviceWidth / 2.5,
-    alignSelf: 'center',
+    height: 300,
+    width: 210,
     borderRadius: 5,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 1,
-      height: 3,
-    },
-    shadowOpacity: 0.5,
-    shadowRadius: 5,
-    marginBottom: 15,
+    marginBottom: 50,
   },
   bookImage: {
     height: '100%',
-    width: '100%',
+    width: 210,
     borderRadius: 5,
+    alignSelf: 'center',
   },
   container: {
-    alignSelf: 'center',
-    width: diviceWidth * 0.95,
-    height: diviceHeight,
+    width: diviceWidth,
     backgroundColor: 'white',
     borderRadius: 15,
     padding: 20,
-    marginBottom: 80,
-  },
-  bookTitle: {
-    fontSize: 25,
-    fontWeight: '700',
+    paddingBottom: 80,
   },
   descMiddleBorder: {
     width: diviceWidth * 0.95,
-    height: 10,
+    height: 5,
     backgroundColor: '#f3f3f3',
     alignSelf: 'center',
+    marginVertical: 15,
   },
   subTitle: { fontSize: 20, fontWeight: '700' },
   chatBox: {
     width: diviceWidth,
     height: 70,
-    backgroundColor: '#4C65FF',
+    backgroundColor: '#438732',
     position: 'absolute',
     bottom: 0,
     borderTopRightRadius: 5,
     borderTopLeftRadius: 5,
   },
   chatBtnText: {
-    fontSize: 23,
-    fontWeight: '600',
+    fontSize: 18,
+    fontFamily: 'SCDream5',
     color: 'white',
     textAlign: 'center',
     padding: 20,
+  },
+  bookCate: { fontFamily: 'SCDream2', fontSize: 12, marginBottom: 5 },
+  bookTitle: { fontFamily: 'SCDream7', fontSize: 18, marginBottom: 5 },
+  bookAuthor: { fontFamily: 'SCDream4', fontSize: 12 },
+  subTitle: { fontFamily: 'SCDream7', fontSize: 14 },
+  bookDesc: { fontSize: 13, lineHeight: 20, marginVertical: 15 },
+  bookStatusBadge: {
+    marginLeft: 10,
+    width: 40,
+    height: 40,
+    borderRadius: 100,
+    borderColor: '#54b65e',
+    backgroundColor: 'white',
+    borderWidth: 5,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 1,
+      height: 3,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+  },
+  commentBox: {
+    backgroundColor: '#E5E9E4',
+    paddingTop: 15,
+    borderRadius: 15,
+    marginTop: 15,
+  },
+  commentInputBox: {
+    height: 50,
+    flexDirection: 'row',
+    backgroundColor: '#438732',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderRadius: 10,
+    margin: 15,
+  },
+  commentInput: {
+    height: '100%',
+    width: '80%',
+    fontSize: 14,
+    paddingHorizontal: 15,
+  },
+  commentBtn: {
+    height: '100%',
+    width: '20%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  commentBtnText: { fontFamily: 'SCDream4', fontSize: 13, color: 'white' },
+  tooltipBtn: {
+    width: 100,
+    height: 30,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  tooltipText: {
+    fontFamily: 'SCDream5',
+    fontSize: 14,
+    color: 'white',
+    textAlign: 'center',
   },
 });
