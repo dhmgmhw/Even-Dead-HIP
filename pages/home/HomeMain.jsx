6bb109ca9a67fd95b2c362fbfceaa7c4ee48725f
dhmgmhw <dhmgmhw@gmail.com>
2021-05-08 23:12:02 +0900

    style: design changing

--- pages/home/HomeMain.jsx
+++ pages/home/HomeMain.jsx
@@ -8,6 +8,7 @@ import {
   Pressable,
   FlatList,
   ActivityIndicator,
+  Image,
 } from 'react-native';
 import { Header } from 'react-native-elements';
 import { Button } from 'native-base';
@@ -16,66 +17,78 @@ import { Ionicons } from '@expo/vector-icons';
 
 import { mocklist } from '../../mock.json';
 import OurTownComponent from '../../components/home/OurTownComponent';
+import GenreComponent from '../../components/home/GenreComponent';
+
+import { getStatusBarHeight } from 'react-native-status-bar-height';
+import { getPostedBook, testGetPost } from '../../config/MainPageApi';
 
 const diviceWidth = Dimensions.get('window').width;
 const diviceHeight = Dimensions.get('window').height;
 
 export default function HomeMain({ navigation }) {
-  const [posts, setPosts] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [pageCurrent, setPageCurrent] = useState(1);
+  const [posts, setPosts] = useState(mocklist);
+
+  const download = async () => {
+    const result = await getPostedBook();
+    setPosts(result);
+  };
 
   useEffect(() => {
-    setPosts(mocklist);
+    const unsubscribe = navigation.addListener('focus', () => {
+      download();
+    });
+    return unsubscribe;
+  }, [navigation]);
+
+  useEffect(() => {
+    download();
   }, []);
-
-  const renderItem = ({ item }) => {
-    return <Text style={{ height: 50 }}>{item.title}</Text>;
-  };
-
-  const renderFooter = () => {
-    return isLoading ? (
-      <View style={styles.loader}>
-        <ActivityIndicator size='small' />
-      </View>
-    ) : null;
-  };
-
-  const handleLoadMore = () => {
-    setPageCurrent(pageCurrent + 1);
-    setIsLoading(true);
-  };
 
   return (
     <>
       <Header
         containerStyle={{
+          paddingHorizontal: 20,
+          borderBottomWidth: 0,
           backgroundColor: 'white',
         }}
         leftComponent={
+          <View
+            style={{
+              flexDirection: 'row',
+            }}>
+            <Text
+              style={{
+                fontSize: 14,
+                fontFamily: 'SCDream5',
+                top: 3,
+                color: '#398E3D',
+              }}>
+              송파구
+            </Text>
+            <Ionicons
+              name={'chevron-down'}
+              size={25}
+              style={{ paddingHorizontal: 5, color: '#398E3D', bottom: 4 }}
+            />
+          </View>
+        }
+        centerComponent={''}
+        rightComponent={
           <Ionicons
             name={'notifications-outline'}
             size={25}
-            style={{ paddingHorizontal: 10 }}
-          />
-        }
-        centerComponent={{
-          text: '송파구',
-          style: { fontSize: 18, fontFamily: 'SCDream5', top: 3 },
-        }}
-        rightComponent={
-          <Ionicons
-            name={'search-outline'}
-            size={25}
-            style={{ paddingHorizontal: 10 }}
+            style={{ color: '#398E3D' }}
           />
         }
       />
-      <ScrollView style={{ backgroundColor: 'white' }}>
+      <ScrollView
+        style={{ backgroundColor: 'white' }}
+        showsVerticalScrollIndicator={false}>
         <View style={styles.mainTitleBox}>
           <View>
-            <Text style={styles.mainTitleText}>우리 동네</Text>
-            <Text style={styles.mainTitleText}>책장</Text>
+            <Text style={styles.mainTitleDesc}>좋은 책을 좋은 이웃과 함께</Text>
+            <Text style={styles.mainTitleText}>우리 동네 책장</Text>
           </View>
           <Pressable
             onPress={() => {
@@ -84,45 +97,30 @@ export default function HomeMain({ navigation }) {
             style={styles.subTitleBtn}>
             <Text
               style={{
-                fontSize: 12,
+                fontSize: 9,
                 fontFamily: 'SCDream6',
                 top: 5,
-                color: 'grey',
+                color: 'white',
               }}>
               더보기{'>'}
             </Text>
           </Pressable>
         </View>
+        <View style={styles.mainTitleDescBox}>
+          <View>
+            <Text style={styles.mainTitleDesc}>
+              지역에서 관심있는 책을 교환하며
+            </Text>
+            <Text style={styles.mainTitleDesc}>
+              내 안의 가치를 같이 키워보세요
+            </Text>
+          </View>
+        </View>
         <View>
-          <ScrollView
-            horizontal={true}
-            showsHorizontalScrollIndicator={false}
-            style={styles.ourTown}>
-            {posts.map((post, i) => {
-              return (
-                <OurTownComponent key={i} navigation={navigation} post={post} />
-              );
-            })}
-          </ScrollView>
           <View style={styles.subTitleBox}>
-            <Text style={{ fontSize: 20, fontFamily: 'SCDream5' }}>
+            <Text style={{ fontSize: 16, fontFamily: 'SCDream6' }}>
               새로 등록된 도서
             </Text>
-            <Pressable
-              onPress={() => {
-                navigation.navigate('OurTownPage');
-              }}
-              style={styles.subTitleBtn}>
-              <Text
-                style={{
-                  fontSize: 12,
-                  fontFamily: 'SCDream6',
-                  top: 5,
-                  color: 'grey',
-                }}>
-                더보기{'>'}
-              </Text>
-            </Pressable>
           </View>
           <ScrollView
             horizontal={true}
@@ -130,24 +128,26 @@ export default function HomeMain({ navigation }) {
             style={styles.newBooksScroll}>
             {posts.map((post, i) => {
               return (
-                <OurTownComponent key={i} navigation={navigation} post={post} />
+                <GenreComponent key={i} navigation={navigation} post={post} />
+              );
+            })}
+          </ScrollView>
+          <View style={styles.borderBox}></View>
+          <View style={styles.subTitleBox}>
+            <Text style={{ fontSize: 16, fontFamily: 'SCDream6' }}>#과학</Text>
+          </View>
+          <ScrollView
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            style={styles.newBooksScroll}>
+            {posts.map((post, i) => {
+              return (
+                <GenreComponent key={i} navigation={navigation} post={post} />
               );
             })}
           </ScrollView>
         </View>
       </ScrollView>
-      <FlatList
-        style={{ borderWidth: 2 }}
-        data={posts}
-        renderItem={renderItem}
-        keyExtractor={(item, index) => index.toString()}
-        ListFooterComponent={renderFooter}
-        onEndReached={handleLoadMore}
-        onEndReachedThreshold={0}
-        horizontal={true}
-        showsHorizontalScrollIndicator={false}
-      />
-
       <Button
         style={styles.addBtn}
         onPress={() => navigation.navigate('AddPage')}>
@@ -161,41 +161,64 @@ const styles = StyleSheet.create({
   mainTitleBox: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    width: diviceWidth,
     height: 100,
     paddingHorizontal: 20,
-    paddingTop: 20,
-    marginTop: 20,
-    backgroundColor: '#F7F6FF',
+    backgroundColor: '#4CB73B',
+    paddingTop: getStatusBarHeight(),
+  },
+  mainTitleDescBox: {
+    height: 70,
+    paddingHorizontal: 20,
+    backgroundColor: '#4CB73B',
+    paddingTop: 10,
+  },
+  mainTitleDesc: {
+    fontFamily: 'SCDream4',
+    fontSize: 14,
+    color: 'white',
+    lineHeight: 20,
   },
   mainTitleText: {
-    fontSize: 24,
-    fontFamily: 'SCDream6',
+    fontSize: 28,
+    fontFamily: 'SCDream7',
     marginBottom: 5,
+    color: '#FFF4BE',
   },
   ourTown: {
-    height: 200,
+    height: 170,
     marginBottom: 20,
-    backgroundColor: '#F7F6FF',
+    backgroundColor: '#4CB73B',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 1,
+      height: 3,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+    marginBottom: 20,
   },
   subTitleBox: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    width: diviceWidth,
     height: 40,
     paddingHorizontal: 20,
     marginTop: 20,
   },
   newBooksScroll: {
-    height: 180,
+    height: 140,
     marginBottom: 20,
   },
   subTitleBtn: {
-    width: 50,
+    width: 35,
     height: 30,
   },
+  borderBox: {
+    height: 5,
+    marginHorizontal: 20,
+    backgroundColor: '#F3F3F3',
+  },
   addBtn: {
-    backgroundColor: '#6864FF',
+    backgroundColor: '#398E3D',
     width: 60,
     height: 60,
     borderRadius: 100,
