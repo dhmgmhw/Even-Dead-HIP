14b66e6c76165c518d2e3f3c7c424581e935e825
dhmgmhw <dhmgmhw@gmail.com>
2021-05-10 15:57:00 +0900

    feat: scrap maker

--- components/home/GenreComponent.jsx
+++ components/home/GenreComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import {
   StyleSheet,
   View,
@@ -7,11 +7,14 @@ import {
   Pressable,
   Text,
 } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 const diviceWidth = Dimensions.get('window').width;
 const diviceHeight = Dimensions.get('window').height;
 
 export default function GenreComponent({ navigation, post }) {
+  const [scrap, setScrap] = useState(false);
+
   return (
     <Pressable
       style={styles.cardBox}
@@ -28,54 +31,72 @@ export default function GenreComponent({ navigation, post }) {
           <View style={styles.cardTitleBox}>
             <View>
               <Text
+                numberOfLines={2}
+                style={{ fontFamily: 'SCDream6', fontSize: 13 }}>
+                {post.title}
+              </Text>
+              <Text
                 numberOfLines={1}
                 style={{
                   fontFamily: 'SCDream3',
                   fontSize: 13,
-                  marginBottom: 10,
+                  marginTop: 10,
                 }}>
-                #{post.category}
-              </Text>
-              <Text
-                numberOfLines={2}
-                style={{ fontFamily: 'SCDream6', fontSize: 13 }}>
-                {post.title}
+                {post.author}
               </Text>
             </View>
             <Text
               numberOfLines={1}
               style={{ fontFamily: 'SCDream3', fontSize: 12 }}>
-              {post.author}
+              송파구
             </Text>
           </View>
         </View>
       </View>
+      <View style={styles.bottomBorder}></View>
+      <Ionicons
+        name={scrap ? 'bookmark' : 'bookmark-outline'}
+        onPress={() => {
+          scrap ? setScrap(false) : setScrap(true);
+        }}
+        size={25}
+        style={[styles.scrap, { color: scrap ? 'green' : 'grey' }]}
+      />
     </Pressable>
   );
 }
 
 const styles = StyleSheet.create({
   cardBox: {
-    borderWidth: 1,
+    marginHorizontal: 20,
+  },
+  bottomBorder: {
+    height: 4,
+    backgroundColor: '#F3F3F3',
   },
   card: {
-    height: 115,
-    marginLeft: 20,
+    height: 95,
+    marginVertical: 20,
   },
   cardFlex: {
     flexDirection: 'row',
   },
   cardImage: {
-    width: 80,
-    height: 120,
+    width: 60,
+    height: 90,
     borderRadius: 5,
     alignSelf: 'center',
   },
   cardTitleBox: {
-    width: 130,
+    width: diviceWidth * 0.7,
     flexDirection: 'column',
     justifyContent: 'space-between',
     marginLeft: 10,
     paddingVertical: 10,
+  },
+  scrap: {
+    position: 'absolute',
+    alignSelf: 'flex-end',
+    top: '60%',
   },
 });
