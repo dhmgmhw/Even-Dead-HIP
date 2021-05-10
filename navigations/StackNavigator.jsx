e1a5c29236b76185dcb9b2519339231c532226e0
dhmgmhw <dhmgmhw@gmail.com>
2021-05-10 21:36:39 +0900

    fix: stack navs

--- navigations/StackNavigator.jsx
+++ navigations/StackNavigator.jsx
@@ -1,17 +1,19 @@
-import React from "react"
-import { createStackNavigator } from "@react-navigation/stack"
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import TabNavigator from "./TabNavigator"
-import SignInPage from "../pages/login/SignInPage"
+import TabNavigator from './TabNavigator';
+import SignInPage from '../pages/login/SignInPage';
 // import SignUpPage from '../pages/login/SignUpPage';
-import PostDetailPage from "../pages/home/PostDetailPage"
-import PostFixPage from "../pages/home/PostFixPage"
-import ChatPage from "../pages/chat/ChatPage"
-import AddPage from "../pages/home/AddPage"
-import OurTownPage from "../pages/home/OurTownPage"
-import MultiAddPage from "../pages/home/MultiAddPage"
+import SignPlusPage from '../pages/login/SignPlusPage';
 
-const Stack = createStackNavigator()
+import PostDetailPage from '../pages/home/PostDetailPage';
+import PostFixPage from '../pages/home/PostFixPage';
+import ChatPage from '../pages/chat/ChatPage';
+import AddPage from '../pages/home/AddPage';
+import OurTownPage from '../pages/home/OurTownPage';
+import MultiAddPage from '../pages/home/MultiAddPage';
+
+const Stack = createStackNavigator();
 
 const StackNavigator = () => {
   return (
@@ -19,17 +21,18 @@ const StackNavigator = () => {
       screenOptions={{
         headerShown: false,
       }}>
-      <Stack.Screen name="SignInPage" component={SignInPage} />
-      <Stack.Screen name="TabNavigator" component={TabNavigator} />
+      <Stack.Screen name='SignInPage' component={SignInPage} />
+      <Stack.Screen name='SignPlusPage' component={SignPlusPage} />
+      <Stack.Screen name='TabNavigator' component={TabNavigator} />
       {/* <Stack.Screen name='SignUpPage' component={SignUpPage} /> */}
-      <Stack.Screen name="OurTownPage" component={OurTownPage} />
-      <Stack.Screen name="MultiAddPage" component={MultiAddPage} />
-      <Stack.Screen name="PostDetailPage" component={PostDetailPage} />
-      <Stack.Screen name="ChatPage" component={ChatPage} />
-      <Stack.Screen name="AddPage" component={AddPage} />
-      <Stack.Screen name="PostFixPage" component={PostFixPage} />
+      <Stack.Screen name='OurTownPage' component={OurTownPage} />
+      <Stack.Screen name='MultiAddPage' component={MultiAddPage} />
+      <Stack.Screen name='PostDetailPage' component={PostDetailPage} />
+      <Stack.Screen name='ChatPage' component={ChatPage} />
+      <Stack.Screen name='AddPage' component={AddPage} />
+      <Stack.Screen name='PostFixPage' component={PostFixPage} />
     </Stack.Navigator>
-  )
-}
+  );
+};
 
-export default StackNavigator
+export default StackNavigator;
