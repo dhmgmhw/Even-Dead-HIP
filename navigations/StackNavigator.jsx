1fc64b0971a6ef37104330b721c2674d99981241
dhmgmhw <dhmgmhw@gmail.com>
2021-05-12 15:51:00 +0900

    feat : change user town page

--- navigations/StackNavigator.jsx
+++ navigations/StackNavigator.jsx
@@ -1,18 +1,21 @@
-import React from "react"
-import { createStackNavigator } from "@react-navigation/stack"
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import TabNavigator from "./TabNavigator"
-import SignInPage from "../pages/login/SignInPage"
-import SignPlusPage from "../pages/login/SignPlusPage"
-import PostDetailPage from "../pages/home/PostDetailPage"
-import PostFixPage from "../pages/home/PostFixPage"
-import ChatPage from "../pages/chat/ChatPage"
-import AddPage from "../pages/home/AddPage"
-import OurTownPage from "../pages/home/OurTownPage"
-import MultiAddPage from "../pages/home/MultiAddPage"
-import CatePage from "../pages/search/CatePage"
+import TabNavigator from './TabNavigator';
+import SignInPage from '../pages/login/SignInPage';
+import SignPlusPage from '../pages/login/SignPlusPage';
+import PostDetailPage from '../pages/home/PostDetailPage';
+import PostFixPage from '../pages/home/PostFixPage';
+import ChatPage from '../pages/chat/ChatPage';
+import AddPage from '../pages/home/AddPage';
+import OurTownPage from '../pages/home/OurTownPage';
+import MultiAddPage from '../pages/home/MultiAddPage';
+import CatePage from '../pages/search/CatePage';
+import TownChangePage from '../pages/home/TownChangePage';
+import TradeSelectPage from '../pages/home/TradeSelectPage';
+import TradeConfirmPage from '../pages/home/TradeConfirmPage';
 
-const Stack = createStackNavigator()
+const Stack = createStackNavigator();
 
 const StackNavigator = () => {
   return (
@@ -21,25 +24,28 @@ const StackNavigator = () => {
         headerShown: false,
       }}>
       <Stack.Screen
-        name="SignInPage"
+        name='SignInPage'
         component={SignInPage}
         options={{ gestureEnabled: false }}
       />
-      <Stack.Screen name="SignPlusPage" component={SignPlusPage} />
+      <Stack.Screen name='SignPlusPage' component={SignPlusPage} />
       <Stack.Screen
-        name="TabNavigator"
+        name='TabNavigator'
         component={TabNavigator}
         options={{ gestureEnabled: false }}
       />
-      <Stack.Screen name="OurTownPage" component={OurTownPage} />
-      <Stack.Screen name="MultiAddPage" component={MultiAddPage} />
-      <Stack.Screen name="PostDetailPage" component={PostDetailPage} />
-      <Stack.Screen name="ChatPage" component={ChatPage} />
-      <Stack.Screen name="AddPage" component={AddPage} />
-      <Stack.Screen name="PostFixPage" component={PostFixPage} />
-      <Stack.Screen name="CatePage" component={CatePage} />
+      <Stack.Screen name='OurTownPage' component={OurTownPage} />
+      <Stack.Screen name='MultiAddPage' component={MultiAddPage} />
+      <Stack.Screen name='PostDetailPage' component={PostDetailPage} />
+      <Stack.Screen name='ChatPage' component={ChatPage} />
+      <Stack.Screen name='AddPage' component={AddPage} />
+      <Stack.Screen name='PostFixPage' component={PostFixPage} />
+      <Stack.Screen name='CatePage' component={CatePage} />
+      <Stack.Screen name='TownChangePage' component={TownChangePage} />
+      <Stack.Screen name='TradeSelectPage' component={TradeSelectPage} />
+      <Stack.Screen name='TradeConfirmPage' component={TradeConfirmPage} />
     </Stack.Navigator>
-  )
-}
+  );
+};
 
-export default StackNavigator
+export default StackNavigator;
