58e23a9ebaa947a07df97f1a575ddf1f45be873a
dhmgmhw <dhmgmhw@gmail.com>
2021-05-14 10:40:30 +0900

    fix : catch errors

--- pages/login/SignInPage.jsx
+++ pages/login/SignInPage.jsx
@@ -38,6 +38,7 @@ export default function SignInPage({ navigation }) {
           navigation.push('TabNavigator');
         } else {
           setReady(false);
+          console.log(err);
         }
       });
     });
